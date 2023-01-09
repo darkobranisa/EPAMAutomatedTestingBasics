202ceec6f14d8c14503a60bcbb18485249798f1c
darkobranisa <97313061+darkobranisa@users.noreply.github.com>
2023-01-09 19:03:56 +0100

    Add files via upload
    Conditional statements and Data types Practical task

---
+++ Conditional Statements and Data Types.js
@@ -0,0 +1,21 @@
+const str = "Name"
+const bool = true
+const nmb = 30
+
+console.log(str + bool)
+console.log(str + nmb)
+console.log(nmb + bool)
+
+console.log(str * bool)
+console.log(str * nmb)
+console.log(nmb * bool)
+
+console.log(str / bool)
+console.log(str / nmb)
+console.log(nmb / bool)
+
+console.log(String(nmb))
+
+console.log(Number(str))
+
+console.log(Boolean(str))
